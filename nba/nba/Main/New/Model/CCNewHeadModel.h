1caab8f2f8433f4314f6b97286376630825eb948
chenchao <chenchao@qq.com>
2016-07-12 19:45:07 +0800

    12陈超的代码

---
+++ nba/nba/Main/New/Model/CCNewHeadModel.h
@@ -0,0 +1,13 @@
+//
+//  CCNewHeadModel.h
+//  nba
+//
+//  Created by lanou3g on 16/7/12.
+//  Copyright © 2016年 wanccao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCNewHeadModel : NSObject
+
+@end
---
+++ nba/nba/Main/New/Request/CCNewRequest.m
@@ -0,0 +1,13 @@
+//
+//  CCNewRequest.m
+//  nba
+//
+//  Created by lanou3g on 16/7/12.
+//  Copyright © 2016年 wanccao. All rights reserved.
+//
+
+#import "CCNewRequest.h"
+
+@implementation CCNewRequest
+
+@end
