eb8d25c535ec272cce944aa973523c05d2852328
chenchao <chenchao@qq.com>
2016-07-13 19:51:54 +0800

    修改

---
+++ nba/nba/Main/New/View/HeadLine/CCHeadLineTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CCHeadLineTableViewCell.h
+//  nba
+//
+//  Created by lanou3g on 16/7/11.
+//  Copyright © 2016年 wanccao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class CCNewHeadModel;
+@interface CCHeadLineTableViewCell : UITableViewCell
+/**  model */
+@property (nonatomic,strong) CCNewHeadModel *model;
+
+@end
